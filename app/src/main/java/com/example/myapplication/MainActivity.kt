d59058d3741d16548d9f33bfa607734b33e26e3b
lytMoon <2936437824@qq.com>
2023-04-29 16:33:42 +0800

    更新了首页标题的toolbar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,63 +1,43 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.example.myapplication.adapter.ViewPager2Adapter
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.fragment.HomeFragment
-import com.example.myapplication.fragment.PlayGroundFragment
-import com.example.myapplication.fragment.UserFragment
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-    //设置fragment集合，用于vp2进行页面跳转绑定
-    val fragmentList: MutableList<Fragment> = ArrayList()
     //懒加载注入databinding
-    private val mBinding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
-        //      下面进行fragment和viewpager的绑定
-        //去除自带的选中颜色,去除后文字和图片选择效果就是跟我们自定义的效果一样
-        mBinding.navView.itemIconTintList = null
-
-        fragmentList.add(HomeFragment())
-        fragmentList.add(PlayGroundFragment())
-        fragmentList.add(UserFragment())//添加3个fragment
-        mBinding.viewPager2.adapter = ViewPager2Adapter(this, fragmentList)
-        //当viewpage2页面切换时nav导航图标也跟着切换
-        mBinding.viewPager2.registerOnPageChangeCallback(object :
-            ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                mBinding.navView.menu.getItem(position).isChecked = true
-            }
-        })
-
-//当nav导航点击切换时，viewpager2也跟着切换页面
-        //mBinding 顺便调用了viewpager2
-        mBinding.navView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.bottom_nav_home -> {
-                    mBinding.viewPager2.currentItem = 0
-                    return@setOnItemSelectedListener true
-                }
-                R.id.bottom_nav_playground -> {
-                    mBinding.viewPager2.currentItem = 1
-                    return@setOnItemSelectedListener true
-                }
-                R.id.bottom_nav_user -> {
-                    mBinding.viewPager2.currentItem = 2
-                    return@setOnItemSelectedListener true
-                }
-
-            }
-            false
-        }
+        setSupportActionBar(mBinding.toolBar)
+        val currentDate = Date()//获取时间
+        val dateFormat = SimpleDateFormat("yyyy/MM", Locale.getDefault())
+        val dateString = dateFormat.format(currentDate)
+        Log.d("555", "(MainActivity.kt:20)-->> " + dateString);
+        // 将日期设置为Toolbar标题
+        mBinding.toolBar.title = "$dateString         新闻简阅"
     }
 
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        when(item.itemId){
+            R.id.menu_user ->{
+                Toast.makeText(this, "您点开了用户", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
 
 
 }
--- app/src/main/java/com/example/myapplication/api/ApiService.kt
+++ app/src/main/java/com/example/myapplication/api/ApiService.kt
@@ -1,25 +1,14 @@
 package com.example.myapplication.api
 
-import com.example.myapplication.myData.UserData
-import com.example.myapplication.myData.UserList
-import retrofit2.Call
-import retrofit2.http.GET
-
 /**
- * description ：用于retrofit的网络请求
+ * description ：
  * author : lytMoon
  * email : yytds@foxmail.com
- * date : 2023/4/25 21:44
+ * date : 2023/4/29 16:28
  * version: 1.0
  */
 interface ApiService {
-    /**
-     * 微信公众号列表
-     * https://wanandroid.com/wxarticle/chapters/json
-     * name关键字可以调用
-     */
-    @GET("wxarticle/chapters/json")
-    fun getwxarticle(): Call<UserList<UserData>>
+
 
 
 }
