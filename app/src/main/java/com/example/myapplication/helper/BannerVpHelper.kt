fca11b8fe125e31c2467c0a26b1a7d6b4716aa1c
lytMoon <2936437824@qq.com>
2023-11-24 23:41:00 +0800

    :art: 完善banner左右滑动

--- app/src/main/java/com/example/myapplication/helper/BannerVpHelper.kt
+++ app/src/main/java/com/example/myapplication/helper/BannerVpHelper.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication.helper
 
+import android.annotation.SuppressLint
 import android.os.Handler
 import android.os.Looper
+import android.view.MotionEvent
 import androidx.viewpager2.widget.ViewPager2
 import com.example.myapplication.api.BannerHelper
 import com.example.myapplication.myData.Story
@@ -10,7 +12,7 @@ import com.example.myapplication.myData.Story
  * 辅助顶部无限轮播图
  * 封装了一个handler
  */
-class BannerVpHelper(val mViewPager: ViewPager2, val mData: List<Story>) : BannerHelper {
+class BannerVpHelper(val mViewPager: ViewPager2, private val mData: List<Story>) : BannerHelper {
 
 
     companion object {
@@ -18,20 +20,16 @@ class BannerVpHelper(val mViewPager: ViewPager2, val mData: List<Story>) : Banne
     }
 
     private lateinit var handler: Handler
-    private var currentItem = 0
+    private var currentItem = mData.size * 100
+    private var isUserScrolling = false
     private var isPaused = false
-
     private val runnable = object : Runnable {
+
         override fun run() {
             //定时任务
             if (!isPaused) {
                 //这里的逻辑有点绕
-                currentItem = mViewPager.currentItem
                 currentItem++
-                //其实就执行一次不是1到一个整体的长度
-                if (currentItem % mData.size == 0) {
-                    currentItem = 0
-                }
                 mViewPager.setCurrentItem(currentItem, true)
             }
             handler.postDelayed(this, DELAY_MILLIS)
@@ -39,11 +37,21 @@ class BannerVpHelper(val mViewPager: ViewPager2, val mData: List<Story>) : Banne
 
     }
 
+
+
+
+
     //开始轮播
     override fun startRun() {
         handler = Handler(Looper.getMainLooper())
         handler.postDelayed(runnable, DELAY_MILLIS)
     }
+
+
+    fun changeCurrentPage(c: Int) {
+        currentItem = c
+    }
+
 
     //结束轮播
     override fun destroyRun() {
